0527c97a05ad4e28f1b52b15d1b345398fbc30d3
Alma <almathompson@protonmail.com>
2024-08-23 09:54:46 -0400

    first crack at "whenever you sacrifice one or more foods" ability

---
+++ Mage.Sets/src/mage/cards/c/CamelliaTheSeedmiser.java
@@ -0,0 +1,85 @@
+package mage.cards.c;
+
+import java.util.UUID;
+import mage.MageInt;
+import mage.constants.SubType;
+import mage.constants.SuperType;
+import mage.constants.TargetController;
+import mage.constants.Zone;
+import mage.counters.CounterType;
+import mage.filter.FilterPermanent;
+import mage.filter.common.FilterControlledCreaturePermanent;
+import mage.filter.predicate.Predicates;
+import mage.filter.predicate.mageobject.AnotherPredicate;
+import mage.filter.predicate.permanent.TokenPredicate;
+import mage.game.permanent.token.SquirrelToken;
+import mage.abilities.Ability;
+import mage.abilities.common.SacrificePermanentTriggeredAbility;
+import mage.abilities.common.SimpleActivatedAbility;
+import mage.abilities.common.SimpleStaticAbility;
+import mage.abilities.costs.common.ForageCost;
+import mage.abilities.costs.mana.ManaCostImpl;
+import mage.abilities.costs.mana.ManaCostsImpl;
+import mage.abilities.effects.common.CreateTokenEffect;
+import mage.abilities.effects.common.continuous.GainAbilityControlledEffect;
+import mage.abilities.effects.common.counter.AddCountersAllEffect;
+import mage.abilities.keyword.MenaceAbility;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.Duration;
+
+
+
+import java.util.UUID;
+
+/**
+ * @author alma555
+ */
+public final class CamelliaTheSeedmiser extends CardImpl {
+
+    private static final FilterPermanent filter = new FilterPermanent(SubType.FOOD, "one or more Foods");
+
+    static {
+        filter.add(TokenPredicate.TRUE);
+    }
+
+    private static final FilterControlledCreaturePermanent filterSquirrels =
+            new FilterControlledCreaturePermanent("Squirrels");
+
+    public CamelliaTheSeedmiser(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{1}{B}{G}");
+
+        this.supertype.add(SuperType.LEGENDARY);
+        this.subtype.add(SubType.SQUIRREL);
+        this.subtype.add(SubType.WARLOCK);
+        this.power = new MageInt(3);
+        this.toughness = new MageInt(3);
+
+        // Menace
+        this.addAbility(new MenaceAbility());
+
+        // Other Squirrels you control have menace.
+        Ability menaceAbility = new SimpleStaticAbility(Zone.BATTLEFIELD,new GainAbilityControlledEffect(
+                new MenaceAbility(false),
+                Duration.WhileOnBattlefield,
+                filterSquirrels,
+                true
+        ));
+        // Whenever you sacrifice one or more Foods, create a 1/1 green Squirrel creature token.
+        Ability ability = new SacrificePermanentTriggeredAbility(
+                new CreateTokenEffect(new SquirrelToken()), filter);
+        this.addAbility(ability);
+    }
+
+    // {2}, Forage: Put a +1/+1 counter on each other Squirrel you control.
+
+    private CamelliaTheSeedmiser(final CamelliaTheSeedmiser card) {
+        super(card);
+    }
+
+    @Override
+    public CamelliaTheSeedmiser copy() {
+        return new CamelliaTheSeedmiser(this);
+    }
+}
